69119f956da1e7a5980c0c87ccb159d6729947bb
jgomezz <jgomezz@gmail.com>
2026-03-29 09:06:49 -0500

    update

--- src/main/java/pe/edu/tecsup/db/categoria/_2_InsertarRegistrosJDBC.java
+++ src/main/java/pe/edu/tecsup/db/categoria/_2_InsertarRegistrosJDBC.java
@@ -1,8 +1,6 @@
 package pe.edu.tecsup.db.categoria;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class _2_InsertarRegistrosJDBC {
 
@@ -17,9 +15,45 @@ public class _2_InsertarRegistrosJDBC {
 
             // Conectarme a la Base de datos
             Connection con = DriverManager.getConnection(URL,USER,PASSWORD);
+
+            // TODO : Ingresar codigo faltante
+
+            // Preparar la sentencia
+            String sql=
+                    """
+                    INSERT INTO 
+                        categorias (nombre, descripcion, orden)
+                    VALUES (?,?,?);
+                    """;
+            PreparedStatement stmt= con.prepareStatement(sql);
+
+            // Preparar los datos a Ingresar
+            stmt.setString(1, "Portatiles");
+            stmt.setString(2, "Portatiles de Gama Alta");
+            stmt.setInt(3, 5);
+
+            // Ejecutar la insercion
+            int estado = stmt.executeUpdate();
+            if (estado != 1)
+                throw new SQLException("No se pudo insertar");
+
+            // Obtener el ID del registro creado
+            int id = 0;
+            sql =
+                """ 
+                    SELECT last_insert_id()
+                """;
+
+            stmt = con.prepareStatement(sql);
+
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next())
+                id = rs.getInt(1);
+
+            System.out.printf("Se inserto el registro de categoria con ID = %d \n", id);
+
+            stmt.close();
             con.close();
-
-
             
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
