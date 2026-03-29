be366cabc00cb52c77fc9b84820704eb0955f37f
jgomezz <jgomezz@gmail.com>
2026-03-29 09:51:01 -0500

    update

--- src/main/java/pe/edu/tecsup/db/categoria/_4_EliminarRegistrosJDBC.java
+++ src/main/java/pe/edu/tecsup/db/categoria/_4_EliminarRegistrosJDBC.java
@@ -2,6 +2,7 @@ package pe.edu.tecsup.db.categoria;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class _4_EliminarRegistrosJDBC {
@@ -17,14 +18,38 @@ public class _4_EliminarRegistrosJDBC {
 
             // Conectarme a la Base de datos
             Connection con = DriverManager.getConnection(URL,USER,PASSWORD);
+
+            // Preparar la sentencia
+            String sql=
+                    """
+                    DELETE FROM  
+                        categorias
+                    WHERE 
+                        id = ?;
+
+                    """;
+            PreparedStatement stmt= con.prepareStatement(sql);
+
+            int id = 6;  // Set del ID del registro a eliminar
+
+            // Preparar el Id del registro a eliminar
+            stmt.setInt(1, id); // id del registro a eliminar
+
+            // Ejecutar la eliminacion
+            int estado = stmt.executeUpdate();
+            if (estado != 1)
+                throw new SQLException("No se pudo eliminar el registro");
+
+            System.out.printf("Se ha eliminado la categoria con id = %d \n", id);
+
+            stmt.close();
             con.close();
 
-
-            
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            //throw new RuntimeException(e);
+            System.err.print(e);
         }
     }
 }
