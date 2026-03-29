bef850b45dc8512c45fd3ad10a760940b8d1e895
jgomezz <jgomezz@gmail.com>
2026-03-29 08:23:11 -0500

    init version

---
+++ src/main/java/pe/edu/tecsup/db/ConsultarRegistrosJDBC.java
@@ -0,0 +1,57 @@
+package pe.edu.tecsup.db;
+
+import java.sql.*;
+
+public class ConsultarRegistrosJDBC {
+
+    public static void main(String[] args) {
+
+        final String URL = "jdbc:mariadb://localhost/almacen2";
+        final String USER = "root";
+        final String PASSWORD = "root";
+
+
+        try {
+            // Cargar el driver
+            Class.forName("org.mariadb.jdbc.Driver");
+
+            // Conectarme a la Base de datos
+            Connection con = DriverManager.getConnection(URL,USER,PASSWORD);
+
+            // Preparar la sentencia
+            String sql=
+                    """
+                    SELECT
+                    	id, nombre, descripcion, orden
+                    FROM
+                    	categorias;
+                    """;
+            PreparedStatement stmt= con.prepareStatement(sql);
+
+            ResultSet rs = stmt.executeQuery();
+
+            while(rs.next()) {
+
+                int id= rs.getInt("id");
+                String nombre = rs.getString("nombre");
+                String desc = rs.getString("descripcion");
+
+                System.out.printf("id = %d , nombre=%s , desc=%s \n", id,nombre,desc);
+            }
+
+            rs.close();
+            stmt.close();
+            con.close();
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+
+
+}
